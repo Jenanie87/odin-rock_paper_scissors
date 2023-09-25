cd83ab0af8043bbd2e223aee92ef53199b16b567
Jenanie87 <107427021+Jenanie87@users.noreply.github.com>
2023-09-25 14:45:03 +0200

    intial commit

---
+++ javascript.js
@@ -0,0 +1,51 @@
+// User Selection
+
+function capitalize(input) {
+  let result = input.toLowerCase();
+  result = result.charAt(0).toUpperCase() + result.slice(1);
+  return result;
+}
+
+// Computer Selection
+
+function getComputerChoice() {
+    
+    let randomComputerChoice = [
+        "Rock",
+        "Paper",
+        "Scissors"
+    ];
+
+    let computerChoice = 
+    randomComputerChoice[Math.floor(Math.random()*randomComputerChoice.length)];
+    return computerChoice;
+}
+
+//Compare the choices and declare the winner
+
+function showMessage(playerSelection, computerSelection) {
+if ( (playerSelection === "Rock" && computerSelection === "Scissors") 
+    || (playerSelection === "Paper" && computerSelection === "Rock")
+    || (playerSelection === "Scissors" && computerSelection === "Paper") ) {
+    alert (`You Win! ${playerSelection} beats ${computerSelection} :)`);
+} else if ( (playerSelection === "Rock" && computerSelection === "Paper")
+|| (playerSelection === "Paper" && computerSelection === "Scissors")
+|| (playerSelection === "Scissors" && computerSelection === "Rock") ) {
+    alert (`You Lose! ${computerSelection} beats ${playerSelection} :(`);
+} else {
+    alert ("Draw!");
+}
+}
+
+function game() {
+    let showUser = prompt('Choose "Rock", "Paper" or "Scissors":');
+    let playerSelection = capitalize(showUser);
+
+    let computerSelection = getComputerChoice();
+    alert (`The Computer chooses ${computerSelection}`);
+    showMessage(playerSelection, computerSelection);
+}
+
+for (let i = 0; i < 5; i++) {
+    game();
+}
