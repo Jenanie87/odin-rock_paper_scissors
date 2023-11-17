179c4e2552e4bf0e120e13d196675402b4d42a04
Jenanie87 <107427021+Jenanie87@users.noreply.github.com>
2023-11-17 12:32:14 +0100

    add button animation

--- javascript.js
+++ javascript.js
@@ -8,17 +8,34 @@ startButton.addEventListener("click", () => {
 }
 )
 
-
 let playerScore = 0;
 let ComputerScore = 0;
 
 // User Selection
 
-function capitalize(input) {
+
+const rockButton = document.querySelector(".rock");
+rockButton.addEventListener("click", function(event) {
+    buttonAnimation(rockButton);
+    console.log(event.target.innerText);
+})
+
+const paperButton = document.querySelector(".paper");
+paperButton.addEventListener("click", function(event) {
+    buttonAnimation(paperButton);
+    console.log(event.target.innerText);
+})
+
+const scissorsButton = document.querySelector(".scissors");
+scissorsButton.addEventListener("click", function(event) {
+    buttonAnimation(scissorsButton);
+    console.log(event.target.innerText);
+})
+/* function capitalize(input) {
   let result = input.toLowerCase();
   result = result.charAt(0).toUpperCase() + result.slice(1);
   return result;
-}
+} */
 
 // Computer Selection
 
@@ -38,14 +55,14 @@ function getComputerChoice() {
 //Compare the choices and declare the winner
 
 function showMessage(playerSelection, computerSelection) {
-if ( (playerSelection === "Rock" && computerSelection === "Scissors") 
-    || (playerSelection === "Paper" && computerSelection === "Rock")
-    || (playerSelection === "Scissors" && computerSelection === "Paper") ) {
+if ( (playerSelection === rockButton && computerSelection === "Scissors") 
+    || (playerSelection === paperButton && computerSelection === "Rock")
+    || (playerSelection === scissorsButton && computerSelection === "Paper") ) {
         playerScore++;
     alert (`You Win! ${playerSelection} beats ${computerSelection} :)`);
-} else if ( (playerSelection === "Rock" && computerSelection === "Paper")
-|| (playerSelection === "Paper" && computerSelection === "Scissors")
-|| (playerSelection === "Scissors" && computerSelection === "Rock") ) {
+} else if ( (playerSelection === rockButton && computerSelection === "Paper")
+|| (playerSelection === paperButton && computerSelection === "Scissors")
+|| (playerSelection === scissorsButton && computerSelection === "Rock") ) {
     ComputerScore++;
     alert (`You Lose! ${computerSelection} beats ${playerSelection} :(`);
 } else {
@@ -54,22 +71,31 @@ if ( (playerSelection === "Rock" && computerSelection === "Scissors")
 }
 
 function game() {
-    let showUser = prompt('Choose "Rock", "Paper" or "Scissors":');
-    let playerSelection = capitalize(showUser);
+/*     let showUser = prompt('Choose "Rock", "Paper" or "Scissors":');
+    let playerSelection = capitalize(showUser); */
 
     let computerSelection = getComputerChoice();
     alert (`The Computer chooses ${computerSelection}`);
     showMessage(playerSelection, computerSelection);
 }
 
+game();
 
-for (let i = 0; i < 5; i++) {
+/* for (let i = 0; i < 5; i++) {
     game();
-}
+} */
 
 // Show score
 
-alert (`Players Score: ${playerScore} \nComputers Score: ${ComputerScore}`);
+/* alert (`Players Score: ${playerScore} \nComputers Score: ${ComputerScore}`);
+ */
 
+// Change the Button by pressing it
 
+function buttonAnimation (button) {
+    button.classList.add("pressed");
 
+    setTimeout(function(){
+        button.classList.remove("pressed");
+    }, 100);
+}
